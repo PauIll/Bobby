8ed0eb717cde0a86115e0efa046cf5f055ec6d01
PauIll <44305927+PauIll@users.noreply.github.com>
2018-10-22 19:06:57 +0200

    Create rocade.py

---
+++ rocade.py
@@ -0,0 +1,181 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import datetime
+import time
+import requests
+import os
+
+
+client = discord.Client()
+nbplayer = 32                                                    #Variable du nombre de joueurs connectés
+nblimit = 30
+wlplayer = []
+wlcrash = []
+wlplayerid = []
+wlcrashid = []
+channelbot = '502555286574202880' #rocade                       #Channel où le bot affiche les listes
+channelref = '503155396920213507' #communication                #Channel de référence pour communiquer avec le bot
+channelweb = '502510555618344960' #nb-joueur
+channelhisto = '502789558656696321' #historique                 #Channel d'historique
+cRole = "Citoyens"                                                #cRole (Common Role) Défini le role lambda qui interagit avec le Bot
+textmp = "```Bonjour à toi, je suis Bobby, c'est moi qui gère la Rocade de Sandy Island.```\n\nComment m'utiliser : \n\n :one: Quand le serveur est complet tu dois m'envoyer le message : **!enter** via le channel spécial qui va te permettre de rejoindre la File d'attente. \n \n :two: Si tu étais sur le serveur mais que tu as crash ou time out tu peux m'envoyer : **!crash** via le channel spécial qui va te permettre de rejoindre la File d'attente **Prioritaire**.\n \n :three: Dès que tu as réussi à te connecter n'oublies surtout pas de m'envoyer : **!quit** via le channel spécial qui va permettre de libérer la File d'attente ! \n \n :warning: __**Tu ne dois pas essayer de te connecter si quelqu'un d'autre est numéro 1 de la File d'attente**__ :warning: \n \n ```Bonne journée à toi !```"
+URL = "http://37.187.158.139:30120/players.json"
+
+@client.event
+async def on_ready():
+    global nbplayer
+    t = datetime.datetime.now()
+    await client.change_presence(status=discord.Status.dnd, game= discord.Game(name="le péage (connecting...)", type=3))
+    await client.send_message(client.get_channel(channelhisto),f"**{t.hour}:{t.minute:02}** : Le BOT à bien démarré :grin:")
+    print("Bobby is ready!")
+   
+@client.event
+async def on_message(message):
+    global nbplayer
+    if message.author == client.user:
+        return
+    
+    role_names = [role.name for role in message.author.roles]   #Vérifie les roles d'un utilisateur
+    channeltyping = message.channel.id                          #channel où est posté le message
+    bot_status = discord.Status.idle                            #Initialise l'état du bot
+    r = requests.get(url = URL)                                 #Récupère toutes les infos "joueur" du serveur FiveM
+    data = r.json()                                             #On garde que les infos
+    long = len(data)                                            #Mise dans une liste des infos
+    nbplayer = long                                             #On Change la variable nbplayer par la taille de la liste
+    name = message.author.nick
+    t = datetime.datetime.now()
+    
+    if "!enter" in message.content and cRole in role_names and channeltyping == channelref:
+        #if message.author.nick not in wlplayer:
+        wlplayer.append((message.author.nick))
+        wlplayerid.append((message.author))
+        print("oui")
+        await client.send_message(client.get_channel(channelhisto),f"**{t.hour}:{t.minute:02}** : {name} est entré dans la rocade :white_check_mark: ")
+        await client.purge_from(client.get_channel(channelbot), limit=100, check=None, before=None, after=None, around=None)
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        t = datetime.datetime.now()
+        await client.send_message(client.get_channel(channelbot),f" :warning: __**Etat de la rocade à : {t.hour}:{t.minute:02}**__ :warning: ")
+
+        if len(wlcrash) > 0 :
+            await client.send_message(client.get_channel(channelbot),f"**Liste Prioritaire :**")
+        for k in range(len(wlcrash)):
+            await client.send_message(client.get_channel(channelbot),f"```{k+1}: {wlcrash[k]} P```")
+                                       
+        if len(wlplayer) > 0 :
+            await client.send_message(client.get_channel(channelbot),f"**Liste de Connexion :**")
+            print("oui")
+        for i in range(len(wlplayer)):
+            await client.send_message(client.get_channel(channelbot),f"```{i+1}: {wlplayer[i]}```")
+        return
+    
+    if "!crash" in message.content and cRole in role_names and channeltyping == channelref:
+        #if message.author.nick not in wlcrash:
+        wlcrash.append((message.author.nick))
+        wlcrashid.append((message.author))
+        await client.send_message(client.get_channel(channelhisto),f"**{t.hour}:{t.minute:02}** : {name} est entré dans la rocade (*Crash*) :warning:")
+        
+        if len(wlplayer) > 0 and len(wlcrash) == 1:
+            await client.send_message(wlplayerid[0],f":warning: C'est désormais **{wlcrash[0]}** qui est prioritaire à cause de son crash, tu dois attendre qu'il se connecte :grin:")
+
+        await client.purge_from(client.get_channel(channelbot), limit=100, check=None, before=None, after=None, around=None)
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        t = datetime.datetime.now()
+        await client.send_message(client.get_channel(channelbot),f" :warning: __**Etat de la rocade à : {t.hour}:{t.minute:02}**__ :warning: ")
+       
+        if len(wlcrash) > 0 :
+            await client.send_message(client.get_channel(channelbot),f"**Liste Prioritaire :**")
+        for y in range(len(wlcrash)):
+            await client.send_message(client.get_channel(channelbot),f"```{y+1}: {wlcrash[y]} P```")
+            
+        if len(wlplayer) > 0 :
+            await client.send_message(client.get_channel(channelbot),f"**Liste de Connexion :**")
+        for j in range(len(wlplayer)):
+            await client.send_message(client.get_channel(channelbot),f"```{j+1}: {wlplayer[j]}```")
+        return
+
+    if "!help" in message.content and cRole in role_names and channeltyping == channelref:
+        if message.author == client.user:
+            return
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        await client.send_message(message.author,textmp)
+        return
+
+    if "!quit" in message.content and cRole in role_names and channeltyping == channelref:
+        if message.author.nick in wlcrash:
+            wlcrash.remove((message.author.nick))
+            wlcrashid.remove((message.author))
+            await client.send_message(client.get_channel(channelhisto),f"**{t.hour}:{t.minute:02}** : {name} a quitté la rocade :negative_squared_cross_mark:")
+            
+        if message.author.nick in wlplayer:
+            wlplayer.remove((message.author.nick))
+            wlplayerid.remove((message.author))
+            await client.send_message(client.get_channel(channelhisto),f"**{t.hour}:{t.minute:02}** : {name} a quitté la rocade :negative_squared_cross_mark:")
+            
+        await client.purge_from(client.get_channel(channelbot), limit=100, check=None, before=None, after=None, around=None)
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        
+        t = datetime.datetime.now()                     #Défini la variable de temps
+        
+        await client.send_message(client.get_channel(channelbot),f" :warning: __**Etat de la rocade à : {t.hour}:{t.minute:02}**__ :warning: ")
+        
+        if len(wlcrash) == 0 and len(wlplayer) == 0:
+            await client.send_message(client.get_channel(channelbot)," *Personne n'est dans la rocade* :grin: ")
+   
+        if len(wlcrash) > 0 :
+             await client.send_message(client.get_channel(channelbot),f"**Liste Prioritaire :**")
+        for y in range(len(wlcrash)):
+            await client.send_message(client.get_channel(channelbot),f"```{y+1}: {wlcrash[y]} P```")
+            
+        if len(wlplayer) > 0 :
+             await client.send_message(client.get_channel(channelbot),f"**Liste de Connexion :**")
+        for j in range(len(wlplayer)):
+            await client.send_message(client.get_channel(channelbot),f"```{j+1}: {wlplayer[j]}```")
+        return
+
+    if "!nbj" in message.content and channeltyping == channelweb:
+        if nbplayer < nblimit :
+            await client.change_presence(status=discord.Status.dnd, game= discord.Game(name=f"le péage ({nbplayer} joueurs)", type=3))
+            return
+        await client.change_presence(status=discord.Status.online, game= discord.Game(name=f"gérer la Rocade ({nbplayer} joueurs)", type=0))
+        return
+        
+    if "!mp" in message.content and nbplayer < 31:
+        if len(wlplayer) > 0 or len(wlcrash) > 0 :
+            if len(wlcrash) > 0 :
+                await client.send_message(wlcrashid[0],":wave: **Rappel** : Tu es premier de la File d'attente, tu peux te connecter ! Penses à envoyer un **!quit** dès que tu es connecté ! Merci bien :grin:")
+                return
+            await client.send_message(wlplayerid[0],":wave: **Rappel** : Tu es premier de la File d'attente, tu peux te connecter ! Penses à envoyer un **!quit** dès que tu es connecté ! Merci bien :grin:")
+            print("oui")
+            return
+
+    if "!clear" in message.content and channeltyping == channelref :
+        await client.purge_from(client.get_channel(channelbot), limit=100, check=None, before=None, after=None, around=None)
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        await client.send_message(client.get_channel(channelbot),f" :warning: __**Etat de la rocade à : {t.hour}:{t.minute:02}**__ :warning: ")
+        await client.send_message(client.get_channel(channelbot)," *Personne n'est dans la rocade* :grin: ")
+        await client.send_message(client.get_channel(channelhisto),f"**{t.hour}:{t.minute:02}** : {name} a clear la rocade")
+        del wlcrash[:]
+        del wlcrashid[:]
+        del wlplayer[:]
+        del wlplayerid[:]
+        return
+        
+
+    if "!variablechange" in message.content and channeltyping == channelref :
+        var = re.findall(r'\d+',message.content)
+        nbplayer = var[0]
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        await client.send_message(message.author,f"Le nombre de joueur en ligne est bien passé à {var[0]}")
+        print(var)
+        return
+
+    if channeltyping == channelref and "!nbjadmin" in message.content:
+        print(nbplayer)
+        await client.purge_from(client.get_channel(channelref), limit=1, check=None, before=None, after=None, around=None)
+        await client.send_message(message.author,f"Il y a **{long} joueur(s)** en ligne")
+        return
+        
+
+client.run(os.getenv('TOKEN'))
